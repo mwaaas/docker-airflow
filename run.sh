bab449d5d725130e3e770aceb1dc1223d0b54281
mwas <francismwangi152@gmail.com>
2016-06-20 09:48:26 +0300

    airflow home directory to be defined on environments not in docker command

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-# airflow needs a home, ~/airflow is the default,
-# but you can lay foundation somewhere else if you prefer
-# (optional)
-export AIRFLOW_HOME=/usr/src/app
-
-sleep 10
 # initialize the database
 airflow initdb
 
-sleep 5
 # start the web server, default port is 8080
 airflow webserver -p 80
